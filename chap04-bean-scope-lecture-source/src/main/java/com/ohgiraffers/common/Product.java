39f898b4f9cbaea3df009a4f2734ee1baa83185a
HyeonggyuKang <kin0204@naver.com>
2025-09-05 14:22:41 +0900

    Scop 어노테이션 활용
    1. singleton , prototype 을 결정 할 수 있으며 디폴트는 singleton으로 생성 됨
    2. prototype : 클라이언트에 bean을 요청할 때마다 새로운 객체를 생성해서 반환

---
+++ chap04-bean-scope-lecture-source/src/main/java/com/ohgiraffers/common/Product.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.common;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+    private String name;
+    private int price;
+
+}
